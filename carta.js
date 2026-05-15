ff843316cc047418184ce75b3276a45b73313d86
Aldana Colomer <aldiicolomer@gmail.com>
2026-05-15 19:17:51 -0300

    .

---
+++ carta.js
@@ -0,0 +1,9 @@
+class Carta{
+    constructor(code, value, suit, imagen){
+
+        tihs.code = code;
+        this.value = value;
+        this.suit = suit;
+        this.imagen = imagen;
+    }
+}
